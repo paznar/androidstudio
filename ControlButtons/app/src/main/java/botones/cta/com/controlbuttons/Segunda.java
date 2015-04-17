f1dd8c1ac4342e1a32135061374eba8650841d96
paznar <pablito_azn@hotmail.com>
2015-04-17 10:26:57 +0200

    botones

---
+++ ControlButtons/app/src/main/java/botones/cta/com/controlbuttons/Segunda.java
@@ -0,0 +1,19 @@
+package botones.cta.com.controlbuttons;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+/**
+ * Created by cta on 17/04/2015.
+ */
+public class Segunda extends ActionBarActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.segunda);
+    }
+}
---
+++ ControlButtons/app/src/main/java/botones/cta/com/controlbuttons/Tercera.java
@@ -0,0 +1,17 @@
+package botones.cta.com.controlbuttons;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by cta on 17/04/2015.
+ */
+public class Tercera extends ActionBarActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tercera);
+    }
+}
